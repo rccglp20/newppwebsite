42e2205deecff2a9459cb22c0bb6d5704ba6788a
Tim <tim.soladoye@gmail.com>
2024-03-10 21:51:59 +0100

    prevent google crawling and export to registrations

--- registrations_basic_table.php
+++ registrations_basic_table.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Data Table with Export</title>
+    <title>Praise Party Registrations</title>
     <meta name="robots" content="noindex, nofollow">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable();
+        });
+    </script>
     <style>
         th, td {
             padding: 15px;
@@ -22,9 +20,9 @@
 </head>
 <body>
 
-<h2>Data Table with Export</h2>
+<h2>Praise Party Registrations</h2>
 
-<table id="dataTable" class="display" style="width:100%">
+<table id="dataTable">
     <thead>
     <tr>
         <th>Name</th>
@@ -56,7 +54,7 @@
     }
 
     // SQL query to fetch data from the database
-    $sql = "SELECT `id`, `name`, `email`, `phone`, `gender`, `province`, `member`, `parish`, `code`, `created_at` FROM praise_party_registrations";
+    $sql = "SELECT `id`,`name`, `email`, `phone`, `gender`, `province`, `member`, `parish`, `code`, `created_at` FROM praise_party_registrations";
     $result = $conn->query($sql);
 
     // Output data of each row
@@ -77,20 +75,6 @@
     ?>
     </tbody>
 </table>
-
-<script>
-    $(document).ready(function() {
-        $('#dataTable').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'excelHtml5',
-                'csvHtml5',
-                'pdfHtml5',
-                'print'
-            ]
-        });
-    });
-</script>
 
 <?php
 // Close connection
